ce27daad2277f3038fd963891f9d5b1507a517a4
hajar1997 <ibrahimlihecer97@gmail.com>
2022-09-12 02:50:55 +0400

    first day

---
+++ src/components/layouts/MainSection.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SearchInput from '../SearchInput';
+
+export const MainSection = () => {
+return (
+<div>
+    <section className='mainSec vh-100 container'>
+        <div className="w-75">
+        <h1>Use Domain Finder to find unique domains!</h1>
+        <p className='fs-6 main-p'>Millions of people are searching for companies, domains, meta tags, company names and etc., on Domain finder.
+            Try today! <b>1 000 000</b> data for your search.</p>
+        </div>
+        <SearchInput />
+    </section>
+</div>
+)
+}
