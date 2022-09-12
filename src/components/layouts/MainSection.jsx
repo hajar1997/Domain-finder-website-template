20b45923f09c6f9a84c6a588f7174b502cfa1d9e
hajar1997 <ibrahimlihecer97@gmail.com>
2022-09-13 01:12:49 +0400

    updated

--- src/components/layouts/MainSection.jsx
+++ src/components/layouts/MainSection.jsx
@@ -6,8 +6,8 @@ return (
 <div>
     <section className='mainSec vh-100 container'>
         <div className="w-75">
-        <h1>Use Domain Finder to find unique domains!</h1>
-        <p className='fs-6 main-p'>Millions of people are searching for companies, domains, meta tags, company names and etc., on Domain finder.
+        <h1 className='main-h1'>Use Domain Finder to find unique domains!</h1>
+        <p className='fs-6 main-p'>Millions of people are searching for companies, domains, meta tags, company names and etc., on <br /> Domain finder.
             Try today! <b>1 000 000</b> data for your search.</p>
         </div>
         <SearchInput />
