003176ecbdb2efd61df2f1622186eaa7c2284aaa
hajar1997 <ibrahimlihecer97@gmail.com>
2022-09-13 16:20:32 +0400

    updated

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import { Form, Input } from 'reactstrap';
+import {Input,
+Button
+} from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown} from '@fortawesome/free-solid-svg-icons'
 
+function SearchInput() {
+return (
+<div className="wrapper mt-4">
+        <Button className='dropdown-style d-flex align-items-center text-wrap' >Resources
+        <FontAwesomeIcon className='ms-3' icon={faChevronDown} />
+        </Button>
+    <Input className='input-style w-75 text-wrap' placeholder='Search something...'/>
+</div>
+);
 
-export default class SearchInput extends React.Component {
-
-    render() {
-        return (
-        <div className='search-input mt-4'>
-            <div className="dropdown-btn">
-                <h2 className="dropdown-h2">Resources</h2>
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M884 256h-75c-5.1 0-9.9 2.5-12.9 6.6L512 654.2 227.9 262.6c-3-4.1-7.8-6.6-12.9-6.6h-75c-6.5 0-10.3 7.4-6.5 12.7l352.6 486.1c12.8 17.6 39 17.6 51.7 0l352.6-486.1c3.9-5.3.1-12.7-6.4-12.7z"></path></svg>
-                
-            </div>
-            <div className="inputOfSearch">
-            <Form>
-            <Input className='nosubmit' placeholder="Search something..." />
-            </Form>
-            </div>
-         </div>
-         );
-    }
 }
+
+export default SearchInput;
